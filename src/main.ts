de997d91107c7bfad34837470804cd189a271a1d
TheMarxian <themarxian@gmail.com>
2025-05-04 15:10:42 +0200

    Try correct GH actions #2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
-import '@/style.css';
+import './style.css';
 import App from '@/App.vue';
 import routes from '@/routes';
 
